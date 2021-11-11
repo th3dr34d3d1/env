5c6d873a6da1aaa44c48c2153088bcda90d1b5c5
th3dr34d3d1 <ajoers@gmail.com>
2021-11-11 17:04:24 -0500

    Added bashrc

--- install.sh
+++ install.sh
@@ -7,3 +7,9 @@ if [ -f ~/.vimrc ]  || [ -L ~/.vimrc ]; then
     rm ~/.vimrc
 fi
 ln -s $DIR/.vimrc ~/.vimrc
+
+echo "Creating symlink for ~/.bashrc"
+if [ -f ~/.vimrc ]  || [ -L ~/.bashrc ]; then
+    rm ~/.bashrc
+fi
+ln -s $DIR/.bashrc ~/.bashrc
